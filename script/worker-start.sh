db93f9956b9deea82932ee64014a20f06fecb4b4
kanzihuang <kanzihuang@qq.com>
2025-01-12 17:05:46 +0800

    feat: worker-start script

---
+++ script/worker-start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+project_dir=$(cd "$(dirname $0)/.." && pwd)
+# shellcheck disable=SC2016
+go run "$project_dir/main.go" \
+    worker -t image-copy-huaweicloud \
+    -a 'skopeo-copy=skopeo copy "$source" "$destination"' \
+    -a 'gzip=gzip "$file"' \
+    -a 'obsutil-copy=obsutil cp "$source" "$destination"'
