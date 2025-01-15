7a5d9ee8acee9a04dba4f7eb5e26e70d32c19647
huangwanli <huwangwl@nancalcloud.com>
2025-01-15 12:35:50 +0800

    feat: obsutil create share

--- script/worker-start.sh
+++ script/worker-start.sh
@@ -6,4 +6,5 @@ go run "$project_dir/main.go" \
     worker -t image-copy-huaweicloud \
     -a 'skopeo-copy=skopeo copy "$source" "$destination"' \
     -a 'gzip=gzip "$file"' \
-    -a 'obsutil-copy=obsutil cp "$source" "$destination"'
+    -a 'obsutil-copy=obsutil cp "$source" "$destination"' \
+    -a 'obsutil-share=obsutil create-share -ac "$access_code" -vp "$validity"'
